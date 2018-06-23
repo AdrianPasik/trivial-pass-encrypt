3e5f1164e43ee0ca5b17340c4f751263d52006b9
Adrian Pasik <AdrianPasik@users.noreply.github.com>
2018-06-23 23:49:55 +0200

    calling encryptor module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-const outputPrint = require('./outputPrint.js');
-const encryptor = require('./swapCharEncrypt.js');
+const outputPrintModule = require('./outputPrint.js');
+const encryptModule = require('./swapCharEncrypt.js');
 
 console.log('Hello nodejs world');
 
-const test = new outputPrint();
+const test = new outputPrintModule();
 console.log( test.printTextBelowNumbers('something really long'));
+//const anotherTest = new encrypt();
+console.log( encryptModule.encrypt({
+    numberOffset: 1,
+    textOffset: 1
+},"abc"));
--- src/swapCharEncrypt.js
+++ src/swapCharEncrypt.js
@@ -1,10 +1,28 @@
 class SwapCharEncrypt {
     static encrypt(config, text) {
-        const letters = "abcdefghijklmnoprstuwxyz";
-        const numbers = "0123456789";
+        let encryptedText = '';
+        const letters = [...'abcdefghijklmnoprstuwxyz'];
+        const numbers = [...'0123456789'];
+
+        const getChar = function(table, singleCharacter, offset) {
+            let index = table.indexOf(singleCharacter) + offset;
+            while(index > numbers.length) {
+                index -= numbers.length;
+            }
+            return table[index];
+        };
 
         for(let singleChar of text) {
-            
+            if(Number.isInteger(singleChar)) {
+                encryptedText += getChar(numbers, singleChar, config.numberOffset);
+            }
+            if(singleChar.match(/[a-z][A-Z]/i)){
+                encryptedText += getChar(letters, singleChar, config.textOffset);
+            }
         }
+
+        return encryptedText;
     }
 }
+
+module.exports = SwapCharEncrypt;
