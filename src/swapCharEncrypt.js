82969c1dcb05622712d579e7f8575a385365b036
Adrian Pasik <AdrianPasik@users.noreply.github.com>
2018-06-27 21:14:18 +0200

    fix to uppercase bug

--- src/swapCharEncrypt.js
+++ src/swapCharEncrypt.js
@@ -2,6 +2,7 @@ class SwapCharEncrypt {
     static encrypt(config, text) {
         let encryptedText = '';
         const letters = [...'abcdefghijklmnoprstuwxyz'];
+        const upperLetters = [...'ABCDEFGHIJKLMNOPRSUWXYZ'];
         const numbers = [...'0123456789'];
 
         const getChar = function(table, singleCharacter, offset) {
@@ -13,13 +14,19 @@ class SwapCharEncrypt {
         };
 
         for(let singleChar of text) {
-            
+            console.log(singleChar);
             if(Number.isInteger(singleChar)) {
                 encryptedText += getChar(numbers, singleChar, config.numberOffset);
             }
-            if(singleChar.match(/[a-z]|[A-Z]/i)){
-                encryptedText += getChar(letters, singleChar, config.textOffset);
+            if(singleChar.match(/[a-z]/i)){
+                if(singleChar == singleChar.toUpperCase()) {
+                    encryptedText += getChar(upperLetters, singleChar, config.textOffset);
+                } else {
+                    encryptedText += getChar(letters, singleChar, config.textOffset);
+                }
+                continue;
             }
+            
         }
         
         return encryptedText;
